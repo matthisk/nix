3e1436d3489923ffa2ac9e719a97ebee6f01c6a8
Matthisk <matthisk@miro.com>
2024-02-21 15:09:07 +0100

    Add basic home manager config

--- lib/mkSystem.nix
+++ lib/mkSystem.nix
@@ -10,14 +10,23 @@ name:
 let
   machineConfig = ../machines/${name}.nix;
   userOSConfig = ../users/${user}/${ if darwin then "darwin" else "nixos" }.nix;
+  userHMConfig = ../users/${user}/home-manager.nix;
 
   # Use the nix-darwin function to create system configuration.
   systemFunc = if darwin then inputs.darwin.lib.darwinSystem else nixpkgs.lib.nixosSystem;
+  home-manager = if darwin then inputs.home-manager.darwinModules else inputs.home-manager.nixosModules;
 in systemFunc rec {
   inherit system;
 
   modules = [
     machineConfig
     userOSConfig
+    home-manager.home-manager {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPkgs = true;
+        home-manager.users.${user} = import userHMConfig {
+            inputs = inputs;
+        };
+    }
   ];
 }
---
+++ users/matthisk/home-manager.nix
@@ -0,0 +1,27 @@
+{ inputs }:
+
+{ config, lib, pkgs, ... }:
+
+let
+  isDarwin = pkgs.stdenv.isDarwin;
+  isLinux = pkgs.stdenv.isLinux;
+in  {
+    home.stateVersion = "23.11";
+    
+    programs.fish = {
+        enable = true;
+
+        # Setup git aliases
+        shellAliases = {
+          ga = "git add";
+          gc = "git commit";
+          gco = "git checkout";
+          gcp = "git cherry-pick";
+          gdiff = "git diff";
+          gl = "git prettylog";
+          gp = "git push";
+          gs = "git status";
+          gt = "git tag";
+        };
+    };
+}
