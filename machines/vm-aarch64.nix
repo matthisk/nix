499d06b5acb991978fe43a9aef8778f21c6358fa
Matthisk Heimensen <m@tthisk.nl>
2024-03-14 15:51:15 +0100

    Fix vmware guest on aarch64

--- machines/vm-aarch64.nix
+++ machines/vm-aarch64.nix
@@ -1,5 +1,13 @@
-{ config, pkgs, lib, ... }: {
-  imports = [ ./hardware/vm-aarch64.nix ./vm-shared.nix ];
+{ config, pkgs, lib, vmwareGuestUnstable, ... }: {
+  # Disable the 23.11 version of vmware-guest module as it is not compatible with aarch64
+  disabledModules = [ "virtualisation/vmware-guest.nix" ];
+
+  imports = [
+    ./hardware/vm-aarch64.nix
+    ./vm-shared.nix
+    # Now include our customer version of vmware guest module.
+    ../modules/vmware-guest.nix
+  ];
 
   # Setup qemu so we can run x86_64 binaries
   boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
@@ -13,5 +21,5 @@
 
   # This works through our custom module imported above
   virtualisation.vmware.guest.enable = true;
-  virtualisation.vmware.guest.headless = true;
+  # virtualisation.vmware.guest.headless = true;
 }
